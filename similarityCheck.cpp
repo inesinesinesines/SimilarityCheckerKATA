a0e9fc3a8dbfe473bd607644170e2e1a45a5130e
inhae.kang <kang.inhae@gmail.com>
2025-07-23 15:27:54 +0900

    [feature]fix length check Calculation type

--- similarityCheck.cpp
+++ similarityCheck.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 class SimLength {
 public :
-	int getResult(string str1, string str2)
+	double getResult(string str1, string str2)
 	{
-		int len1 = str1.length();
-		int len2 = str2.length();
+		double len1 = str1.length();
+		double len2 = str2.length();
 
 		if (len1 < len2) std::swap(len1, len2);
 
-		return (1 - ((len1 - len2) / len2)) * 60;
+		double rate = (len1 - len2) / len2;
+		double result = (1 - rate) * 60.0;
+		return result;
 	}
 };
 
--- similarityCheck_test.cpp
+++ similarityCheck_test.cpp
@@ -8,7 +8,7 @@ TEST(CheckSimularity, SameLength)
 	std::string str1 = "ABCD";
 	std::string str2 = "ABCD";
 	SimLength simlength;
-	int result = simlength.getResult(str1, str2);
+	double result = simlength.getResult(str1, str2);
 
 	EXPECT_EQ(60, result);
 }
@@ -18,7 +18,7 @@ TEST(CheckSimularity, SameLengthDiffChar)
 	std::string str1 = "DSA";
 	std::string str2 = "ASD";
 	SimLength simlength;
-	int result = simlength.getResult(str1, str2);
+	double result = simlength.getResult(str1, str2);
 
 	EXPECT_EQ(60, result);
 }
@@ -28,21 +28,21 @@ TEST(CheckSimularity, 2xLengthDiffChar)
 	std::string str1 = "A";
 	std::string str2 = "BB";
 	SimLength simlength;
-	int result = simlength.getResult(str1, str2);
+	double result = simlength.getResult(str1, str2);
 
 	EXPECT_EQ(0, result);
 }
+
 
 TEST(CheckSimularity, LenDiff5to3)
 {
 	std::string str1 = "AAABB";
 	std::string str2 = "BAA";
 	SimLength simlength;
-	int result = simlength.getResult(str1, str2);
+	double result = simlength.getResult(str1, str2);
 
 	EXPECT_LT(result, 60);
 }
-
 
 int main() {
 	::testing::InitGoogleMock();
